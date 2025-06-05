b4f4cd4975f4401079f0e8adf4090fc6d1a39b28
Algecom <contact@algecom.com>
2025-06-05 17:21:23 +0100

    Update index.ts

--- v1/middleware/response/index.ts
+++ v1/middleware/response/index.ts
@@ -2,12 +2,14 @@ import { Elysia } from "elysia";
 
 export default (app: Elysia) => app
 .onError(({ error }) => {
+  console.log({ error });
   return {
     success: false,
     error: error instanceof Error ? error.message : String(error)
   };
 })
 .onAfterHandle(({ response }) => {
+  console.log({ response });
   // If the response is already a well-formed success/failure response, leave it alone
   if (
     typeof response === 'object' &&
